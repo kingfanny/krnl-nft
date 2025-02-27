9d09e7623ef4996e349a7e6cd9a55b8bc7d2411f
kingfanny <200625697+kingfanny@users.noreply.github.com>
2025-02-27 11:51:38 +0200

    fix: update deploy script

--- script/KrnlNFT.s.sol
+++ script/KrnlNFT.s.sol
@@ -7,19 +7,12 @@ import {Upgrades} from "openzeppelin-foundry-upgrades/Upgrades.sol";
 
 contract DeployScript is Script {
     function run() external returns (address, address) {
-        //we need to declare the sender's private key here to sign the deploy transaction
-        uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
-        vm.startBroadcast(deployerPrivateKey);
-
-        // Deploy the upgradeable contract
         address _proxyAddress = Upgrades.deployTransparentProxy(
             "KrnlNFT.sol", msg.sender, abi.encodeCall(KrnlNFT.initialize, ("https://example.com/", 1))
         );
 
         // Get the implementation address
         address implementationAddress = Upgrades.getImplementationAddress(_proxyAddress);
-
-        vm.stopBroadcast();
 
         return (implementationAddress, _proxyAddress);
     }
